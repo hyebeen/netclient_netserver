bff5285d8db442c9021033add70e1dd92423460e
hyebeen <llhb3015@naver.com>
2016-03-18 20:22:05 +0900

    111

---
+++ client/client/netclient.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+void ErrorHandling(char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN serverAddr;
+
+	char message[30];
+	int strLen;
+	int result;
+
+	if (argc != 3) //언래 기본값 1 + 2개입력하니까 3개
+	{
+		ErrorHandling("usage error");
+	}
+
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData); //소켓프로그래밍할때 기본적으로 해줘야하는것
+	if (result != 0)
+	{
+		ErrorHandling("WSAStartup() error");
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0); //ipv4 , tcp 소켓 생성
+	if (hSocket == INVALID_SOCKET)
+	{
+		ErrorHandling("socket() error");
+	}
+
+	memset(&serverAddr, 0, sizeof(serverAddr)); //덮어쓸메모리주소, 덮어쓸 값,덮어쓸 사이즈
+	serverAddr.sin_family = AF_INET; //ipv4
+	serverAddr.sin_addr.S_un.S_addr = inet_addr(argv[1]); //ip
+	serverAddr.sin_port = htons(atoi(argv[2])); //포트 atoint 정수형으로 바꿈
+
+	result = connect(hSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)); //서버로의 연결
+	if (result == INVALID_SOCKET) 
+	{
+		ErrorHandling("connect() error");
+	}
+
+	printf("connected!");
+
+	while (1)
+	{
+		printf("보낼 메세지를 입력하세요:");
+		scanf("%s", message);
+		strLen = send(hSocket, message, strlen(message) + 1, 0);
+		
+		if (strLen == -1)
+		{
+			ErrorHandling("recv() error");
+		}
+
+	}
+	
+
+	closesocket(hSocket);
+	WSACleanup();
+
+	system("pause");
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stdout);
+	fputc('\n', stdout);
+	system("pause"); //프로그램이 종료되도 창이 안닫힘
+	exit(1);
+}
---
+++ server/server/netserver.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+void ErrorHandling(char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServerSock, hClientSock;
+	SOCKADDR_IN serverAddr, clientAddr;
+
+	char message[100];
+
+	int sizeClientAddr;
+	int result;
+
+	if (argc != 3)
+	{
+		ErrorHandling("usage error");
+	}
+
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData); //기본적으로 해야하는거
+	if (result != 0)
+	{
+		ErrorHandling("WSAStartup() error");
+	}
+
+	hServerSock = socket(PF_INET, SOCK_STREAM, 0); //소켓만듬
+	if (hServerSock == INVALID_SOCKET)
+	{
+		ErrorHandling("socket() error");
+	}
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(atoi(argv[1]));
+
+	result = bind(hServerSock, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (result == SOCKET_ERROR)
+	{
+		ErrorHandling("bind() error");
+	}
+
+	result = listen(hServerSock, 5); //오는거 기다림 입력이 동시에 몇개 올수있는지
+	if (result == SOCKET_ERROR)
+	{
+		ErrorHandling("listen() error");
+	}
+	//else ErrorHandling("listen() sucess");
+
+	sizeClientAddr = sizeof(clientAddr);
+	hClientSock = accept(hServerSock, (SOCKADDR*)&clientAddr, &sizeClientAddr);
+	if (hClientSock == INVALID_SOCKET)
+	{
+		ErrorHandling("accept() error");
+	}
+
+	//메세지 받아서 화면에 출력
+	while (1)
+	{
+		result = recv(hClientSock, message, 100, 0);
+		if (result > 0)
+		{
+			printf("%s\n", message);
+		}
+	}
+
+	closesocket(hServerSock);
+	closesocket(hClientSock);
+	WSACleanup();
+
+	system("pause");
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stdout);
+	fputc('\n', stdout);
+	system("pause");
+	exit(1);
+
+}
